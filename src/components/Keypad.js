3f101e903eaab37ba5c7e2aaf97661cbe4180f81
Brian Joseph <brianjoseph8132@gmail.com>
2024-11-11 00:20:54 +0300

    add Keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    const handleclick=() =>{
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleclick}></input>
+        </div>
     )
 }
 
